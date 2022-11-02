16623a9c44c75d2f88f7b88a773e2216171cc060
middleware <malifarge.thomas9@gmail.com>
2022-11-02 16:36:04 +0100

    form components

--- src/Pages/SignIn.js
+++ src/Pages/SignIn.js
@@ -1,8 +1,11 @@
+import Form from "../Components/Form"
 
 const SignIn = () => {
     return (
       <div>
-       <h1>SignIn</h1>  
+       <h1>SignIn</h1>
+      <Form type="SignIn"/>
+
       </div>
     )
   }
