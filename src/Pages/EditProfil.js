fc29c6f08c964a624be135a45dd275c45fb5cada
middleware <malifarge.thomas9@gmail.com>
2022-11-04 13:38:37 +0100

    Form correction

--- src/Pages/EditProfil.js
+++ src/Pages/EditProfil.js
@@ -1,6 +1,24 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+
 import EditUser from "../Components/Templates/EditUser"
 
+import { useEffect,useContext } from "react"
+import { useNavigate } from "react-router-dom"
+
+import { UserContext } from "../Context/User"
+
 const EditProfil = () => {
+
+  const{user} = useContext(UserContext)
+    const navigate = useNavigate()
+
+
+  useEffect(()=>{
+    if(!user){
+      navigate("/")
+    }
+  },[])
+
     return <EditUser/>
   }
   
