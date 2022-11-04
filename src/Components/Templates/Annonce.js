caa4a7fb68b7c90d2cdbdfdc84cf5ba05779a53d
WadeTheDev <echo1006j@gmail.com>
2022-11-04 11:46:19 +0100

    Product page updated

--- src/Components/Templates/Annonce.js
+++ src/Components/Templates/Annonce.js
@@ -1,27 +1,29 @@
-// import Card from '../Card'
+import Card from "../Card";
+import SideBar from "../SideBar";
+import { Button } from "@mui/material";
 
-const Annonce = ({product}) => {
-  return <>
-  <div>
-    {/* <h1>{product.Price}</h1>
-    <h2>{product.sender}</h2>
-  </div>
-  <div>
-    <span>{product.description}</span>
-    <span>{product.description}</span>
-    <span>{product.description}</span>
-  </div>
-  <Button  onClick={handleClick} > SEND A MESSAGE</Button>
- <div>
-    <h3>TITLE PRODUCTE</h3>
-    <div>{products.map((product) => {
-      return(
-        <Card product={product}/>
-      )
-    })}
-  </div> */}
-  </div>
-  </>
+const Annonce = ({ product, products }) => {
+  return (
+    <>
+      <SideBar />
+      <div>
+        <h1>{product.productPrice}$</h1>
+        <h2>{product.productName}</h2>
+      </div>
+      <div>
+        <span>{product.productDescription}</span>
+      </div>
+      <Button> SEND A MESSAGE</Button>
+      <div>
+        <h3>TITLE PRODUCTE</h3>
+        <div>
+          {products.map((product) => {
+            return <Card product={product} />;
+          })}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Annonce;
